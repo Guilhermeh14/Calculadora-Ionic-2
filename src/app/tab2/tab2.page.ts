46aeb8012b838133c483734622b386d22490237b
Guilherme Fernando de Souza <guilherme_h14@hotmail.com>
2022-10-04 21:47:25 -0300

    Calculadora  Ionic
    Calculadora Completa com Ionic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  operacao = '';
+  resultado = '';
   constructor() {}
 
+  adicionarValor(valor: string)
+  {
+    this.operacao += valor;
+  }
+
+  limparOperacao()
+  {
+    this.operacao = '';
+  }
+
+  limparMemoria()
+  {
+    this.operacao = '';
+    this.resultado = '';
+  }
+
+  apagarCaracter()
+  {
+    if(this.operacao.length > 0)
+    {
+      this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+  }
 }
